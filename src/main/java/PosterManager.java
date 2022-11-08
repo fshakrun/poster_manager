603ce8f665cf6afb2c3301158741f1e215d82135
Fedor Shakrun <fshakrun@gmail.com>
2022-11-08 13:50:18 +0300

    initial commit

---
+++ src/main/java/PosterManager.java
@@ -0,0 +1,39 @@
+public class PosterManager {
+    private String[] movies = new String[0];
+    private final int limit;
+
+    public PosterManager(){
+        limit = 10;
+    }
+    public PosterManager(int limit){
+        this.limit = limit;
+
+    }
+
+
+    public void add(String movie){
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll(){
+        return movies;
+    }
+    public String[] findLast(){
+        int resulLength;
+        if (movies.length < limit){
+            resulLength = movies.length;
+        } else {
+            resulLength = limit;
+        }
+        String[] tmp = new String[resulLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return  tmp;
+    }
+}
